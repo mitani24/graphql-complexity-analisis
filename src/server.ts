7c1b68cd7178818460d08e497c2d690a2d2711e8
mitani24 <h.mitani@ashita-team.com>
2021-09-10 19:10:39 +0900

    add graphql-query-complexity

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { ApolloServer, gql } from "apollo-server-lambda";
 import resolvers from "./resolvers";
 import BookAPI from "./datasources/book-api";
 import constAnalysis from "graphql-cost-analysis";
+import { createComplexityRule } from "graphql-query-complexity";
 
 const typeDefs = fs
   .readFileSync(path.join(__dirname, "../schema.graphql"))
@@ -20,9 +21,27 @@ const server = new ApolloServer({
   validationRules: [
     constAnalysis({
       defaultCost: 1,
-      maximumCost: 10,
+      maximumCost: 100,
       onComplete(cost: number) {
         console.log(`graphql-cost-analysis: ${cost}`);
+      },
+    }),
+    createComplexityRule({
+      maximumComplexity: 100,
+      estimators: [
+        ({ field, args, childComplexity }) => {
+          if (field.type.toString().includes("Connection")) {
+            const length = args.first ?? 0 + args.last ?? 0;
+            if (childComplexity === 0) {
+              return length;
+            }
+            return length * childComplexity;
+          }
+          return childComplexity;
+        },
+      ],
+      onComplete(complexity) {
+        console.log(`graphql-query-complexity: ${complexity}`);
       },
     }),
   ],
